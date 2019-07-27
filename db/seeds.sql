0db3ed39e89b3eb67d9fbc322d9d1d20682dd09d
jsf5077 <jsf5077@gmail.com>
2019-07-27 19:29:41 -0400

    App Setup. DB Setup. Config Setup. Model Setup.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Smokehouse Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheddar Burger');
+INSERT INTO burgers (burger_name) VALUES ('Upside Down Whopper Burger');
